af4be984e499b6e526d02d2a385a3c16c59838e4
Alumno <Alumno@Mac-16.local>
2025-09-09 16:34:40 -0600

    Agregados archivos de proyecto Xcode

---
+++ PearCoProject/PearCoProject/PearCoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PearCoProjectApp.swift
+//  PearCoProject
+//
+//  Created by Alumno on 09/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PearCoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
