b20f2d141f99fb77a4d57e84a343a0aeddf05b47
Dario Puentes <a00838287@tec.mx>
2025-09-09 19:30:20 -0600

    Pantalla alertas y agricultores (DARIO)

---
+++ PearCoProject/PearCoProject/FarmersView.swift
@@ -0,0 +1,18 @@
+//
+//  FarmersView.swift
+//  PearCoProject
+//
+//  Created by Alumno on 09/09/25.
+//
+
+import SwiftUI
+
+struct FarmersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FarmersView()
+}
