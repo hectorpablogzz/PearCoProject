959a5c62fe9a7701635a03dd9e83a30ccde8884e
Dany <a01198931@tec.mx>
2025-09-10 16:47:46 -0600

    cambiar color del accent

--- PearCoProject/PearCoProject/ContentView.swift
+++ PearCoProject/PearCoProject/ContentView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let verdeOscuro = Color(red: 32/255, green: 75/255, blue: 54/255) // Color de tu app
     var body: some View {
         
         CustomTabView()
-            .accentColor(.green)
+            .accentColor(verdeOscuro)
             .padding(.top, 10)
             .edgesIgnoringSafeArea(.bottom)
         }
